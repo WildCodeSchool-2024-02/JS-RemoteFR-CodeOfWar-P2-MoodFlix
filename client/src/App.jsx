50c6619c1b0085c50bb9812affe4db5ed8da7fc2
dijan cetingoz <dijan.cetingoz@icloud.com>
2024-06-25 17:36:45 +0200

    ✅ test exercice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,12 @@ import { useState } from "react";
 function App() {
   // Déclarer un état pour stocker le titre du film
   const [title, setTitle] = useState("Jurassic");
+  const [changeNumber, setChangeNumber] = useState(11);
 
   // Fonction appelée lors du clic sur le bouton
   const handleClick = () => {
     // 👇 Changer l'URL pour récupérer un film spécifique depuis The Movie DB
-    const url = `https://api.themoviedb.org/3/movie/200?language=fr&api_key=${import.meta.env.VITE_API_KEY}`;
+    const url = `https://api.themoviedb.org/3/movie/${changeNumber}?language=fr&api_key=${import.meta.env.VITE_API_KEY}`;
     
     // Effectuer une requête GET à l'API avec axios
     axios.get(url)
@@ -22,6 +23,10 @@ function App() {
       });
   }
 
+  const handleChange = (event) => {
+    setChangeNumber(event.target.value)
+  }
+
   return (
     <main className="container">
       {/* Bouton pour déclencher la recherche du film */}
@@ -29,7 +34,7 @@ function App() {
       {/* Affichage du titre du film */}
       <p>Titre: {title}</p>
       {/* Exemple d'un autre input */}
-      <input type="number" defaultValue={11}/>
+      <input type="number" onChange={handleChange} defaultValue={11}/>
     </main>
   );
 }
