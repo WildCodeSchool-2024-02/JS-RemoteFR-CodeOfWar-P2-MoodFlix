99b3abf3ec5d3c3a73c99be47c83d3a1473b548e
Benjamin <dilgopierre@gmail.com>
2024-07-01 15:18:08 +0200

    💫 Add images animation ('HomeImages.jsx')

--- client/src/components/HomeImages.jsx
+++ client/src/components/HomeImages.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import fetchMovies from "../utils/fetchMovies";
 import baseImageUrl from "../utils/baseImageUrl";
 import "../styles/HomeImages.css";
@@ -13,11 +14,14 @@ export default function HomeImages() {
   return (
     <section className="home-image">
       {movies
-        ? movies.map((movie) => (
-            <img
+        ? movies.map((movie, index) => (
+            <motion.img
               key={movie.id}
               src={baseImageUrl + movie.poster_path}
               alt={movie.title}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: index * 0.2 }}
             />
           ))
         : null}
