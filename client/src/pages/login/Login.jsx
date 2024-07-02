a17d13486ecd42ecfe4caa95f9c758e18bf6dd73
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-07-02 16:02:58 +0200

    :wrench: Created login folder and Login & LoginForm components

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../../components/Navbar";
+import LoginForm from "./LoginForm";
+
+function Login() {
+    return (
+        <>
+            <div>
+                <Navbar />
+            </div>
+            <div>
+                <LoginForm />
+            </div>
+        </>
+    );
+};
+
+export default Login;
---
+++ client/src/pages/login/LoginForm.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+function LoginForm() {
+    return (
+        <>
+            <div className="login-container">
+                <h2>WELCOME BACK👋</h2>
+                <form action="#">
+                    <label htmlFor="email" name="email">MAIL ADRESS</label>
+                    <input type="email" id="email" />
+
+                    <label htmlFor="password" name="password">PASSWORD</label>
+                    <input type="password" id="password" />
+
+                    <div className="checkbox-container">
+                        <input type="checkbox" name="checkbox" id="checkbox" />
+                        <label htmlFor="checkbox">REMEMBER ME</label>
+                    </div>
+
+                    <div className="forgot-password">
+                        <Link to="/forgot">FORGOT PASSWORD?</Link>
+                    </div>
+
+                    <button type="submit">LOGIN</button>
+
+                </form>
+            </div>
+            <div className="login-img">
+                <img src="" alt="" />
+            </div>
+        </>
+    );
+};
+
+export default LoginForm;
