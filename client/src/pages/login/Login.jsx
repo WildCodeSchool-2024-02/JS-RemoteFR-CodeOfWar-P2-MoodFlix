0be937f1e208b2fb0929b876aaeb8836f6853657
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-07-02 19:31:59 +0200

    :lipstick: Started to add styles on Login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,21 +1,26 @@
 import Navbar from "../../components/Navbar";
 import LoginForm from "./LoginForm";
 import LoginImage from "../../assets/images/social-network.jpeg";
+import "../../styles/Login.css";
 
 function Login() {
-    return (
-        <>
-            <div>
-                <Navbar />
-            </div>
-            <div>
-                <LoginForm />
-            </div>
-            <div className="login-img">
-                <img src={LoginImage} alt="" />
-            </div>
-        </>
-    );
-};
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div className="global-container">
+        <div className="login-container">
+          <div className="form-container">
+            <LoginForm />
+          </div>
+          <div className="login-img">
+            <img src={LoginImage} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Login;
--- client/src/pages/login/LoginForm.jsx
+++ client/src/pages/login/LoginForm.jsx
@@ -1,31 +1,33 @@
 import { Link } from "react-router-dom";
 
 function LoginForm() {
-    return (
+  return (
+    <div className="login-form-container">
+      <h2>WELCOME BACK👋</h2>
+      <form action="#">
+        <label htmlFor="email" name="email">
+          MAIL ADRESS
+        </label>
+        <input type="email" id="email" />
 
-        <div className="login-container">
-            <h2>WELCOME BACK👋</h2>
-            <form action="#">
-                <label htmlFor="email" name="email">MAIL ADRESS</label>
-                <input type="email" id="email" />
+        <label htmlFor="password" name="password">
+          PASSWORD
+        </label>
+        <input type="password" id="password" />
 
-                <label htmlFor="password" name="password">PASSWORD</label>
-                <input type="password" id="password" />
-
-                <div className="checkbox-container">
-                    <input type="checkbox" name="checkbox" id="checkbox" />
-                    <label htmlFor="checkbox">REMEMBER ME</label>
-                </div>
-
-                <div className="forgot-password">
-                    <Link to="/forgot">FORGOT PASSWORD?</Link>
-                </div>
-
-                <button type="submit">LOGIN</button>
-
-            </form>
+        <div className="checkbox-container">
+          <input type="checkbox" name="checkbox" id="checkbox" />
+          <label htmlFor="checkbox">REMEMBER ME</label>
         </div>
-    );
-};
+
+        <div className="forgot-password">
+          <Link to="/forgot">FORGOT PASSWORD?</Link>
+        </div>
+
+        <button type="submit">LOGIN</button>
+      </form>
+    </div>
+  );
+}
 
 export default LoginForm;
