52b4d3a9584774743d35452febc967bf7a2c6af1
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-07-03 14:31:01 +0200

    :mag: Added alt to img tag

--- client/src/pages/sign-up/SignUp.jsx
+++ client/src/pages/sign-up/SignUp.jsx
@@ -4,19 +4,19 @@ import SignUpImage from "../../assets/images/social-network.jpeg";
 import "../../styles/SignUp.css";
 
 function SignUp() {
-  return (
-    <>
-      <div>
-        <Navbar />
-      </div>
-      <div>
-        <SignUpForm />
-      </div>
-      <div className="login-img">
-        <img src={SignUpImage} alt="" />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Navbar />
+            </div>
+            <div>
+                <SignUpForm />
+            </div>
+            <div className="login-img">
+                <img src={SignUpImage} alt="Discussion scene between the main characters in the film The Social Network" />
+            </div>
+        </>
+    );
 }
 
 export default SignUp;
