31fcf47d9fd0c34c8403730ad66336a4e4c2c25b
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-07-03 11:50:02 +0200

    :sparkles: Created a link on Navbar to Login

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import SvgPerson from "../assets/svg/person.svg";
 import SvgPeople from "../assets/svg/people.svg";
@@ -11,10 +12,12 @@ export default function Navbar() {
           <img className="svg person" alt="sign in button" src={SvgPerson} />
           <p>Sign Up</p>
         </button>
-        <button type="button" className="user-buttons login">
-          <img className="svg people" alt="login button" src={SvgPeople} />
-          Login
-        </button>
+        <Link to="/Login">
+          <button type="button" className="user-buttons login">
+            <img className="svg people" alt="login button" src={SvgPeople} />
+            Login
+          </button>
+        </Link>
       </div>
     </nav>
   );
