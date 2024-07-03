98944c68e734184a60427c1c30ae24b910d28ae1
clement Vigouroux <clement.vigouroux@gmail.com>
2024-07-03 15:19:00 +0200

    :wrench: edit proptype

--- client/src/pages/film/Detail.jsx
+++ client/src/pages/film/Detail.jsx
@@ -1,22 +1,38 @@
 import PropTypes from "prop-types";
 import baseImageUrl from "../../utils/baseImageUrl";
+import "../../styles/film/Detail.css";
 
 export default function Detail({film}) {
-  console.info(film.genres)
   return (
     <section className="detail">
-      <h1>{film.title}</h1>
-      <img src={baseImageUrl+film.poster_path} alt={film.title}/>
-       {film.genres.map((genre) => <p key={genre.id}> {genre.name} </p>)}
-       <p>{film.release_date}</p>
-       {film.production_companies.map((companie) => 
-       <div key={companie.id}>
-       <img  src={baseImageUrl+companie.logo_path} alt={companie.name} />
-       <p > {companie.name} </p>
-       </div>)}
+      <div className="leftPart">
+        <h1>{film.title}</h1>
+        <img className="poster" src={baseImageUrl+film.poster_path} alt={film.title}/>
+      </div>
+      <div className="middlePart">
+        {film.genres.map((genre) => <p key={genre.id}> {genre.name} </p>)}
+      </div>
+      <div className="rightPart">
+        <p>{film.release_date}</p>
+        {film.production_companies.map((companie) => 
+        <div key={companie.id}>
+        <p > {companie.name} </p>
+        </div>)}
+      </div>
     </section>
   );
 }
 Detail.propTypes = {
-  film: PropTypes.string.isRequired,
+  film: PropTypes.shape({
+    title : PropTypes.string,
+    poster_path : PropTypes.string,
+    release_date : PropTypes.string,
+    production_companies : PropTypes.arrayOf(PropTypes.shape({
+      name : PropTypes.string,
+    })),
+    genres : PropTypes.arrayOf(PropTypes.shape({
+      id : PropTypes.number,
+      name : PropTypes.string,
+    }))
+    }).isRequired,
 };
