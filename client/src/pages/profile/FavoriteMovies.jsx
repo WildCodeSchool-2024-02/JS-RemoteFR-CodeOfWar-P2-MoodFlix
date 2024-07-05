024aad0e99a61c4bd611d746df0e9f6dfd64a9e1
dijan cetingoz <dijan.cetingoz@icloud.com>
2024-07-05 10:59:20 +0200

    🎨 CSS improved

--- client/src/pages/profile/FavoriteMovies.jsx
+++ client/src/pages/profile/FavoriteMovies.jsx
@@ -5,23 +5,23 @@ function FavoriteMovies() {
       <div id="topFour">
         <img
           className="favMovies"
-          src="https://static.posters.cz/image/350/affiches-et-posters/dune-partie-1-i122815.jpg"
-          alt="affiche dune"
+          src="https://fr.web.img4.acsta.net/img/ef/18/ef18d7a6a51c4a778e7ffa0069015b5c.jpg"
+          alt="affiche fargo"
         />
         <img
           className="favMovies"
-          src="https://static.posters.cz/image/350/affiches-et-posters/dune-partie-1-i122815.jpg"
-          alt="affiche dune"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2GQLBRwSZB_GDmLysGazjvYgf15gyOxT0y_r00hmgJldpC0Tyvi-tfwBPclIpGWBSCsI&usqp=CAU"
+          alt="affiche there will be blood"
         />
         <img
           className="favMovies"
-          src="https://static.posters.cz/image/350/affiches-et-posters/dune-partie-1-i122815.jpg"
-          alt="affiche dune"
+          src="https://a.ltrbxd.com/resized/film-poster/5/5/8/6/4/55864-good-morning-0-230-0-345-crop.jpg?v=1793183742"
+          alt="affiche good morning"
         />
         <img
           className="favMovies"
-          src="https://static.posters.cz/image/350/affiches-et-posters/dune-partie-1-i122815.jpg"
-          alt="affiche dune"
+          src="https://i.ebayimg.com/images/g/IjkAAOSwMWhfP7G4/s-l1600.jpg"
+          alt="affiche the warriors"
         />
       </div>
     </section>
