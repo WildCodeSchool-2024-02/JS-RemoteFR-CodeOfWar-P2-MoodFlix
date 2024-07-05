8a81fb0b87e8f0ebf826142831f8b62b2109d0f5
dijan cetingoz <dijan.cetingoz@icloud.com>
2024-07-05 09:42:28 +0200

    🎨css modified

--- client/src/pages/profile/UserData.jsx
+++ client/src/pages/profile/UserData.jsx
@@ -3,15 +3,19 @@ function UserData() {
     <section className="dataMovies">
       <div className="WatchlistMovies">
         <h2>Watchlist</h2>
+        <p>blababla</p>
       </div>
       <div className="listMovies">
         <h2>list</h2>
+        <p>blababla</p>
       </div>
       <div className="tagMovies">
         <h2>tags</h2>
+        <p>blababla</p>
       </div>
       <div className="friends">
         <h2>amis</h2>
+        <p>blababla</p>
       </div>
     </section>
   );
