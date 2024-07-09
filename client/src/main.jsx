d4226096bb230c0d45d1594e944f595c67b54b1a
Benjamin Daniel-Noury <dilgopierre@gmail.com>
2024-07-09 11:25:45 +0200

    🚨 Fix eslint errors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 import Film from "./pages/film/Film";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import SignUp from "./pages/sign-up/SignUp";
+import Tag from "./pages/tag/Tag";
 import fetchFilm, { fetchCrew, fetchImage } from "./utils/fetchFilm";
+import getMoviesByMood from "./services/getMoviesByMood";
+import "./styles/profileCard.css";
 
 function loaderFilm({ params }) {
   const { id } = params;
@@ -19,21 +26,6 @@ function loaderFilm({ params }) {
     }
   );
 }
-
-// page components
-
-import Profile from "./pages/profile/Profile";
-
-// page styles
-
-import "./styles/profileCard.css";
-
-// router creation
-
-import Login from "./pages/login/Login";
-import SignUp from "./pages/sign-up/SignUp";
-import getMoviesByMood from "./services/getMoviesByMood";
-import Tag from "./pages/tag/Tag";
 
 const router = createBrowserRouter([
   {
