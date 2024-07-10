6bc3dbfcbc5e0547d8e290c3c7aaa296bf6c274d
dijan cetingoz <dijan.cetingoz@icloud.com>
2024-07-10 16:09:46 +0200

    🎨 css modified for Favorite Movies

--- client/src/pages/profile/FavoriteMovies.jsx
+++ client/src/pages/profile/FavoriteMovies.jsx
@@ -1,28 +1,20 @@
+import Movie from "./Movie";
+
 function FavoriteMovies() {
+  const favoriteMovieTitles = [
+    "8358", 
+    "94329", 
+    "7345", 
+    "1084736"
+  ];
+
   return (
     <section>
       <h1 className="firstTitle">Favorite Movies</h1>
       <div id="topFour">
-        <img
-          className="favMovies"
-          src="https://fr.web.img4.acsta.net/img/ef/18/ef18d7a6a51c4a778e7ffa0069015b5c.jpg"
-          alt="affiche fargo"
-        />
-        <img
-          className="favMovies"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2GQLBRwSZB_GDmLysGazjvYgf15gyOxT0y_r00hmgJldpC0Tyvi-tfwBPclIpGWBSCsI&usqp=CAU"
-          alt="affiche there will be blood"
-        />
-        <img
-          className="favMovies"
-          src="https://a.ltrbxd.com/resized/film-poster/5/5/8/6/4/55864-good-morning-0-230-0-345-crop.jpg?v=1793183742"
-          alt="affiche good morning"
-        />
-        <img
-          className="favMovies"
-          src="https://i.ebayimg.com/images/g/IjkAAOSwMWhfP7G4/s-l1600.jpg"
-          alt="affiche the warriors"
-        />
+        {favoriteMovieTitles.map((id) => (
+          <Movie key={id} id={id} />
+        ))}
       </div>
     </section>
   );
---
+++ client/src/pages/profile/Movie.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types"; 
+import axios from "axios";
+
+export default function Movie({ id }) {
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${id}`,
+          {
+            params: {
+              api_key: import.meta.env.VITE_API_KEY,
+            },
+          }
+        );
+        setMovie(response.data);
+      } catch (error) {
+        console.error("Error fetching movie:", error);
+      }
+    };
+
+    fetchMovie();
+  }, [id]);
+
+  return (
+    <>
+      {movie ? (
+        <img
+          className="favMovies"
+          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+          alt="poster"
+        />
+      ) : null}
+      <p>'</p>
+    </>
+  );
+}
+
+Movie.propTypes={
+    id:PropTypes.string.isRequired
+}
