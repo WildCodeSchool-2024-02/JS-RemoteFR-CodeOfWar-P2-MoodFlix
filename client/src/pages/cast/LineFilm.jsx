7fa7084f3f5d800162881f420bc452d48feca0eb
clement Vigouroux <clement.vigouroux@gmail.com>
2024-07-15 17:56:49 +0200

    :sparkles: add link actor to film

--- client/src/pages/cast/LineFilm.jsx
+++ client/src/pages/cast/LineFilm.jsx
@@ -1,20 +1,26 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "../../styles/cast/Cast.css";
 import "../../styles/cast/Filmographie.css";
 import Button from "../../assets/svg/Button.svg";
 
-export default function LineFilm({ title, character }) {
+export default function LineFilm({ film }) {
   return (
     <section>
       <p>
         <img src={Button} alt="point" />
-        {title}
+        <Link to={`/film/${film.id}`} key={film.id}>
+          {film.title}
+        </Link>
       </p>
-      <p className="role">dans le role de : {character}</p>
+      <p className="role">dans le role de : {film.character}</p>
     </section>
   );
 }
 LineFilm.propTypes = {
-  title: PropTypes.string.isRequired,
-  character: PropTypes.string.isRequired,
+  film: PropTypes.shape({
+    title: PropTypes.string,
+    character: PropTypes.string,
+    id: PropTypes,
+  }).isRequired,
 };
