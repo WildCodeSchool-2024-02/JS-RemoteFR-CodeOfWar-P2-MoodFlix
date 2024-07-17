25ae57d09458189ee0b06445ebe821ff21e68d6f
Benjamin Daniel-Noury <dilgopierre@gmail.com>
2024-07-17 11:52:05 +0200

    :hammer: Add body of profile

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,11 +1,32 @@
+import { useLoaderData } from "react-router-dom";
 import Layout from "../../components/layout/Layout";
-import "../../styles/profile/Profile.css";
 import Header from "./Header.profile";
+import Section from "./Section.profile";
+import FilmProfile from "./Film.profile";
+import "../../styles/profile/Profile.css";
 
 function Profile() {
+  const data = useLoaderData();
+
+  console.info(data);
   return (
     <Layout className="profile">
       <Header />
+      <main>
+        <Section title="Favorite Films">
+          {data.slice(0, 4).map((film) => (
+            <FilmProfile film={film} key={film.title} />
+          ))}
+        </Section>
+        <Section title="RECENT ACTIVITY">
+          {data.slice(4, 8).map((film) => (
+            <FilmProfile film={film} key={film.title} />
+          ))}
+        </Section>
+        <Section title="WATCHLIST">???</Section>
+        <Section title="LISTES">???</Section>
+        <Section title="tags">???</Section>
+      </main>
     </Layout>
   );
 }
