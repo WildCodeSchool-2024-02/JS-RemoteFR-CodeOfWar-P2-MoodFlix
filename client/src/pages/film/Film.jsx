29b526dedaf4c00e74635e456b25faa7c8f03c98
clement Vigouroux <clement.vigouroux@gmail.com>
2024-07-18 12:10:04 +0200

    :rewind: change the delete of Image in mobile

--- client/src/pages/film/Film.jsx
+++ client/src/pages/film/Film.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 import Detail from "./Detail";
 import Crew from "./Crew";
@@ -7,31 +6,13 @@ import Image from "./Image";
 import "../../styles/film/film.css";
 import Layout from "../../components/layout/Layout";
 
-const useWindowDimensions = () => {
-  const [dimensions, setDimensions] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setDimensions({ width: window.innerWidth, height: window.innerHeight });
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  return dimensions;
-};
-
 function Film() {
   const { film } = useLoaderData();
-  const { width } = useWindowDimensions();
 
   return (
     <div className="filmContainer">
       <Layout className="film">
-        {width >= 480 ? <Image /> : null}
+        <Image />
         <Detail film={film} />
         <section className="secondPart">
           <Synopsis />
