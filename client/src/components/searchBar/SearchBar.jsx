3cf443361fd95af272c04b5ef0a4d9873e2b43fd
Benjamin Daniel-Noury <dilgopierre@gmail.com>
2024-07-18 10:39:56 +0200

    :sparkles: Add select with arrow to search bar

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -9,23 +9,40 @@ const suggestionsList = Object.keys(moodTable);
 export default function SearchBar() {
   const [search, setSearch] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [oldSearch, setOldSearch] = useState("");
+  const [selectedSuggestionIndex, setSelectedSuggestionIndex] = useState(-1);
 
   const handleChange = (event) => {
     const newSearch = event.target.value;
     setSearch(newSearch);
+    setOldSearch(newSearch);
 
     if (newSearch.length > 0) {
       const filteredSuggestions = suggestionsList.filter((suggestion) =>
         suggestion.toLowerCase().includes(newSearch.toLowerCase())
       );
       setSuggestions(filteredSuggestions);
+      setSelectedSuggestionIndex(-1);
     } else {
       setSuggestions([]);
     }
   };
 
   const handleKeyChange = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === "ArrowDown") {
+      setSelectedSuggestionIndex((prevIndex) =>
+        Math.min(prevIndex + 1, suggestions.length - 1)
+      );
+      setSearch(suggestions[selectedSuggestionIndex + 1]);
+    } else if (event.key === "ArrowUp") {
+      setSelectedSuggestionIndex((prevIndex) => {
+        const newIndex = Math.max(prevIndex - 1, -1);
+        setSearch(
+          newIndex === -1 ? oldSearch : suggestions[selectedSuggestionIndex - 1]
+        );
+        return newIndex;
+      });
+    } else if (event.key === "Enter") {
       window.location.href = `/mood/${findSimilarMood(search)}`;
     }
   };
@@ -41,8 +58,13 @@ export default function SearchBar() {
       />
       {suggestions.length ? (
         <ul>
-          {suggestions.map((suggestion) => (
-            <Suggestion key={suggestion}>{suggestion}</Suggestion>
+          {suggestions.map((suggestion, index) => (
+            <Suggestion
+              key={suggestion}
+              className={index === selectedSuggestionIndex ? "selected" : ""}
+            >
+              {suggestion}
+            </Suggestion>
           ))}
         </ul>
       ) : null}
