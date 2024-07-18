3cc98426f07efee12bac20d8cb4dedf255c1d0f4
Benjamin Daniel-Noury <dilgopierre@gmail.com>
2024-07-18 11:46:26 +0200

    :sparkles: Add link to home film

--- client/src/pages/home/HomeImages.jsx
+++ client/src/pages/home/HomeImages.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 import fetchMovies from "../../utils/fetchMovies";
 import baseImageUrl from "../../utils/baseImageUrl";
 import "../../styles/HomeImages.css";
@@ -15,14 +16,18 @@ export default function HomeImages() {
     <section className="home-image">
       {movies
         ? movies.map((movie, index) => (
-            <motion.img
-              key={movie.id}
-              src={baseImageUrl + movie.poster_path}
-              alt={movie.title}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: index * 0.2 }}
-            />
+            <Link key={movie.id} to={`/film/${movie.id}`}>
+              <motion.img
+                src={baseImageUrl + movie.poster_path}
+                alt={movie.title}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: index * 0.2 },
+                }}
+              />
+            </Link>
           ))
         : null}
     </section>
